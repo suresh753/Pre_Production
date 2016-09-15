82cf8562f189b6aee530d89ba9bb7bd18f4fd3b9
suresh753 <kasthurisuri53@gmail.com>
2016-09-15 15:47:30 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,36 @@
+from exceptions import Exception
+lines=[]
+total=0
+fail=0
+import yaml
+with open('config.yaml','r') as content:
+    data = yaml.load(content)
+Req_Test_Per = data["servers"]["testenv"]
+with open ('log.txt','rt') as in_file:
+    for line in in_file:
+        lines.append(line.rstrip('\n'))
+index = 0
+for element in lines:
+    substr = "Tests run:"
+    index = element.find(substr, index)
+    if index>=0:
+        total_start = element.find(':',index)
+        total_end = element.find(',',total_start)
+        fail_start=element.find(':',total_end)
+        fail_end = element.find(',',fail_start)
+        total=int(element[total_start+2:total_end])
+        fail=int(element[fail_start+2:fail_end])
+        break
+    index=0
+print("TestCases Run :" +str(total))
+print("Failures :" +str(fail))
+Out_Per = (float(total-fail)/total)*100
+print("Percentage :"+str(Out_Per)+"%")
+
+if  Out_Per >= Req_Test_Per:
+    print ("Passed for deploying in testenv")
+else:
+    raise Exception("build failed because of Required Percentage "+str(Req_Test_Per)+"%")
+
+
+    
